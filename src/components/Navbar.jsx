a198c8ed6172871d6fea9345a0724dba40c6946c
Snehatk2002 <sammu5285@gmail.com>
2026-05-07 13:12:15 +0530

    study-in-Albania

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,54 +2,91 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">
-          Study Albania
+
+        {/* LOGO */}
+
+        <Link className="navbar-brand fw-bold fs-4" to="/">
+          🎓 Study Albania
         </Link>
+
+        {/* TOGGLE BUTTON */}
 
         <button
           className="navbar-toggler"
+          type="button"
           data-bs-toggle="collapse"
           data-bs-target="#nav"
+          aria-controls="nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
+
           <span className="navbar-toggler-icon"></span>
+
         </button>
 
-        <div className="collapse navbar-collapse" id="nav">
-          <ul className="navbar-nav ms-auto">
+        {/* NAVBAR LINKS */}
+
+        <div
+          className="collapse navbar-collapse"
+          id="nav"
+        >
+
+          <ul className="navbar-nav ms-auto text-center">
+
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link fw-semibold" to="/">
                 Home
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/universities">
+              <Link
+                className="nav-link fw-semibold"
+                to="/universities"
+              >
                 Universities
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/courses">
+              <Link
+                className="nav-link fw-semibold"
+                to="/courses"
+              >
                 Courses
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link
+                className="nav-link fw-semibold"
+                to="/about"
+              >
                 About
               </Link>
             </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/admin">
+            <li className="nav-item mt-2 mt-lg-0 ms-lg-3">
+
+              <Link
+                className="btn btn-warning rounded-pill px-4"
+                to="/admin"
+              >
                 Admin
               </Link>
+
             </li>
+
           </ul>
+
         </div>
+
       </div>
+
     </nav>
   );
 }
